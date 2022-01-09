7c4d10f51c745f663bd1274a13ec10ffe8cb6ac1
BJL156 <bjl15634@gmail.com>
2022-01-09 10:20:46 -0500

    Commented Code

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def set_up():
     print('Youtube Video and Audio Downloader\n'
         'BJL156\n')
     url = input('Enter a URL: ')
-    # never gonna give you up: https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    # never gonna give you up test link: https://www.youtube.com/watch?v=dQw4w9WgXcQ
 
     yt = YouTube(url)
 def main():
@@ -17,13 +17,17 @@ def main():
     audio = yt.streams.filter(only_audio = True).first()
 
 
+    # remove any characters that can't be in a folder
     folder_name = yt.title.replace('[', '')
     folder_name = folder_name.replace(']', '')
     folder_name = re.sub('[\/:*?"<>|「」]', '', folder_name)
+
+    # can't make a folder if the name of the folder is longer than 156
     while len(folder_name) > 156:
         folder_name = folder_name[:-1]
     os.mkdir(yt.title)
     
+    # can't make the file if the name of the file is longer than 68
     while len(folder_name) > 68:
         folder_name = folder_name[:-1]
         print(len(folder_name))
